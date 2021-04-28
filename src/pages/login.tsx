e25494b7e2de9627594a95aa92b17b07d98d07a1
Kazuki Tobita <kazukiti201@gmail.com>
2021-04-29 02:41:51 +0900

    Modify login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,15 @@
 const Login = () => (
   <div>
     <h1>ログイン</h1>
+    <label>
+      メールアドレス
+      <input type="text" />
+    </label>
+    <label>
+      パスワード
+      <input type="password" />
+    </label>
+    <button type="submit">ログイン</button>
   </div>
 )
 
