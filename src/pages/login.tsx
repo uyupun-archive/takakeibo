e34b1863f12ca3476ec2c1ce537fd2819d78e560
Kazuki Tobita <kazukiti201@gmail.com>
2021-04-29 03:40:29 +0900

    Modify login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,37 @@
-const Login = () => (
-  <div>
-    <h1>ログイン</h1>
-    <label>
-      メールアドレス
-      <input type="text" />
-    </label>
-    <label>
-      パスワード
-      <input type="password" />
-    </label>
-    <button type="submit">ログイン</button>
-  </div>
-)
+import {useState} from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = e => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={login}>
+        <label>
+          メールアドレス
+          <input
+            type="text"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          パスワード
+          <input
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">ログイン</button>
+      </form>
+    </div>
+  )
+}
 
 export default Login;
