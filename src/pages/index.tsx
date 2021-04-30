87c312fa5ef67880b2413c3b75a77fff798b990f
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-01 01:25:47 +0900

    Modify index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,34 @@
 import {useEffect, useState} from 'react';
 import {useRouter} from 'next/router';
-import {auth} from '../lib/firebase';
+import {auth, db} from '../lib/firebase';
 
 const Index = () => {
   const router = useRouter();
+  const financesCollectRef = db.collection('finances');
+
+  const [uid, setUid] = useState(null);
+  const [finances, setFinances] = useState(null);
 
   const checkIsLoggedIn = () => {
     auth.onAuthStateChanged(user => {
       if (!user) return router.push('/login');
-      console.log(user);
-    })
+      setUid(user.uid);
+    });
   };
 
   const logout = e => {
     auth.signOut();
-  }
+  };
+
+  const fetchFinances = () => {
+    if (uid)
+      financesCollectRef.doc(uid).get().then(doc => {
+        if (doc.exists) setFinances(doc.data().data);
+      });
+  };
 
   useEffect(checkIsLoggedIn, []);
+  useEffect(fetchFinances, [uid]);
 
   return (
     <div>
@@ -24,6 +36,28 @@ const Index = () => {
       <form onSubmit={logout}>
         <button type="submit">ログアウト</button>
       </form>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>日付</th>
+            <th>カテゴリ</th>
+            <th>収入</th>
+            <th>支出</th>
+          </tr>
+        </thead>
+        <tbody>
+          {finances && finances.map((finance, idx) => {
+            return (
+              <tr key={idx}>
+                <td>{finance.traded_at}</td>
+                <td>{finance.category}</td>
+                <td>{finance.income}</td>
+                <td>{finance.expenditure}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
