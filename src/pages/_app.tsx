3eceb2cf91bd547b52aaddad1bb64b4119e83601
Kazuki Tobita <kazukiti201@gmail.com>
2021-05-01 01:40:00 +0900

    Add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
