6f075514c3f25a67588871b63c1e9cab70e46c1e
takashi0602 <takashioicsn@gmail.com>
2021-05-20 23:55:26 +0900

    Add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,35 @@
+import {ReactNode} from 'react';
+
+interface Props {
+  children: ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+  size?: 'small' | 'medium';
+  color?: 'blue' | 'red' | 'gray';
+  customClass?: string;
+  onClick?: () => void;
+}
+
+const Button = (props: Props) => {
+  const {children, type, size, color, customClass, onClick} = props;
+
+  let sizeClass: string = 'px-10';
+  if (size === 'medium') sizeClass = 'px-16';
+
+  let colorClass: string = 'bg-blue-500 hover:bg-blue-700 focus:ring-blue-300';
+  if (color === 'red') colorClass = 'bg-red-500 hover:bg-red-700 focus:ring-red-300';
+  else if (color === 'gray') colorClass = 'bg-gray-500 hover:bg-gray-700 focus:ring-gray-300';
+
+  return (
+    <button
+      type={type || 'button'}
+      className={`${sizeClass} ${colorClass} ${customClass || ''} text-white font-bold py-2 px-10 rounded focus:outline-none focus:ring-2`}
+      onClick={() => {
+        if (onClick) onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export {Button};
