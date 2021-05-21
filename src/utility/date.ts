ca0f90e0798d57dbeed5ba16d99bc59a04ed3f10
takashi0602 <takashioicsn@gmail.com>
2021-05-22 01:23:13 +0900

    Refactor method reordering

--- src/utility/date.ts
+++ src/utility/date.ts
@@ -8,12 +8,12 @@ export const convertYearMonth = (d: string): string => {
   return `${ary[0]}-${ary[1]}`;
 };
 
-const zeroPadding = (n: number) => {
-  if (n <= 9) return `0${n}`;
-  return `${n}`;
-}
-
 export const convertMonthDay = (d: string): string => {
   const ary = d.split('-');
   return `${ary[1]}-${ary[2]}`;
-}
+};
+
+const zeroPadding = (n: number) => {
+  if (n <= 9) return `0${n}`;
+  return `${n}`;
+};
