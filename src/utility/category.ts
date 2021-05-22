f435e2b01cfed9dbd8099433b1f5e7d67d516f4a
takashi0602 <takashioicsn@gmail.com>
2021-05-22 23:31:08 +0900

    Refactor delete utility/category.ts

--- src/utility/category.ts
+++
@@ -1,5 +0,0 @@
-import {Category} from '../models/category';
-
-export const convertIdToNameOfCategory = (categories: Array<Category>, categoryId: number): string => {
-  return (categories.find((category) => category.id === categoryId))?.name || ''
-};
