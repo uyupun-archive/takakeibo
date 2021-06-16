3b75894cc2b39d44c1b2441859ae78a7f097534c
Kazuki Tobita <kazukiti201@gmail.com>
2021-06-16 15:59:30 +0900

    Modify _app.tsx type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import Head from 'next/head';
 import '../styles/globals.css'
+import type {AppProps} from 'next/app';
+import Head from 'next/head';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
